294884f3dc59201f609c6cce1bc4ba8bd5ccf869
Alexis Brian Mango <alexi@frba.utn.edu.ar>
2023-08-29 16:04:44 -0300

    Punto 4 con recursividad

--- Logico/hogwarts.pl
+++ Logico/hogwarts.pl
@@ -1,60 +1,114 @@
 %%%%%%%%%%% Parte 1
-%mago(Mago,Sangre,CasaQueOdia)
-mago(harry,mestizo,odia(slytherin)).
-mago(draco,puro,odia(hufflepuff)).
-mago(hermione,muggle,_).
+%mago(Mago,Sangre)
+mago(harry,mestizo).
+mago(draco,puro).
+mago(hermione,muggle).
+mago(neville,muggle).
+
+odiariaIr(harry,slytherin).
+odiariaIr(draco,hufflepuff).
 
 caracteristicas(harry,[corajudo,amistoso,orgulloso,inteligente]).
 caracteristicas(draco,[inteligente,orgulloso]).
 caracteristicas(hermione,[inteligente,orgulloso,responsable]).
+caracteristicas(neville,[corajudo,amistoso]).
 
+% Punto 1
 esSangrePura(Sangre):-
     Sangre \= muggle.
 
-% Punto 1
 permiteEntrar(Mago,slytherin):-
-    mago(Mago,Sangre,_),
+    mago(Mago,Sangre),
     esSangrePura(Sangre).
 
 permiteEntrar(Mago, Casa):-
-    mago(Mago,_,_),
-    Casa \= slytherin.
+    mago(Mago,_),
+    Casa\=slytherin.
 
 % Punto 2
 puedeIrA(Mago,gryffindor):-
-    mago(Mago,_,_),
+    mago(Mago,_),
     caracteristicas(Mago,Caracteristicas),
     member(corajudo,Caracteristicas).
 
 puedeIrA(Mago,slytherin):-
-    mago(Mago,Sangre,_),
+    mago(Mago,Sangre),
     caracteristicas(Mago,Caracteristicas),
     member(orgulloso,Caracteristicas),
     member(inteligente,Caracteristicas),
     esSangrePura(Sangre).
 
 puedeIrA(Mago,ravenclaw):-
-    mago(Mago,_,_),
+    mago(Mago,_),
     caracteristicas(Mago,Caracteristicas),
     member(responsable,Caracteristicas),
     member(inteligente,Caracteristicas).
 
 puedeIrA(Mago,hufflepuff):-
-    mago(Mago,_,_),
-    esAmistoso(Mago,Caracteristicas).
+    mago(Mago,_),
+    esAmistoso(Mago).
 
-esAmistoso(Mago,Caracteristicas):-
+esAmistoso(Mago):-
     caracteristicas(Mago,Caracteristicas),
     member(amistoso,Caracteristicas).
 
 % Punto 3
 enQueCasaQueda(Mago,Casa):-
-    mago(Mago,Sangre,CasaQueOdia),
+    mago(Mago,Sangre),
     puedeIrA(Mago,Casa),
     permiteEntrar(Mago,Casa),
-    CasaQueOdia\=Casa.
+    not(odiariaIr(Mago,Casa)).
+
+enQueCasaQueda(hermione,gryffindor).
 
 % Punto 4
-cadenaDeAmistades(Magos):-
-    findall(Mago,esAmistoso(Mago,Caracteristicas),Magos).
 
+cadenaDeAmistades(Magos):-
+    todosAmistosos(Magos),
+    cadenaDeCasas(Magos).
+
+todosAmistosos(Magos):-
+    forall(member(Mago,Magos), esAmistoso(Mago)).
+
+cadenaDeCasas([Mago1,Mago2] | OtrosMagos):-
+    puedeIrA(Mago1,Casa),
+    puedeIrA(Mago2,Casa),
+    cadenaDeCasas([Mago2 | OtrosMagos]).
+
+cadenaDeCasas([_]).
+
+cadenaDeCasas([]).
+%%%%%%%%%%% Parte 2
+
+esDe(hermione, gryffindor).
+esDe(ron, gryffindor).
+esDe(harry, gryffindor).
+esDe(draco, slytherin).
+esDe(luna, ravenclaw).
+
+accion(harry,resta(fueraDeCama)).
+accion(harry,resta(irAlTercerPiso)).
+accion(harry,resta(irAlBosque)).
+accion(harry,suma(ganarleAVoldy)).
+accion(ron,suma(ganarEnAjedrez)).
+accion(hermione,resta(irAlTercerPiso)).
+accion(hermione,resta(irABibliotecaRestringida)).
+accion(draco,resta(irAMazmorras)).
+
+% Punto 1
+esBuenAlumno(Mago,Accion):-
+    esDe(Mago,_),
+    accion(Mago,suma(_)).
+
+% Punto 2
+accionRecurrente(Accion):-
+    accion(Mago,Accion),
+    accion(OtroMago,Accion),
+    Mago \= OtroMago.
+
+% Punto 3
+/*ganadorDeLaCopa(Casa):-
+    esDe(Mago,Casa),
+    puntosTotales(Casa).*/
+
+%puntosTotales(Casa):-
