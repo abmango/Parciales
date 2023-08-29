40033b92f73ca14f5056155b886085799090c130
Alexis Brian Mango <alexi@frba.utn.edu.ar>
2023-08-29 12:15:36 -0300

    Realizada parte 1.

--- Logico/hogwarts.pl
+++ Logico/hogwarts.pl
@@ -1,3 +1,5 @@
+%%%%%%%%%%% Parte 1
+%mago(Mago,Sangre,CasaQueOdia)
 mago(harry,mestizo,odia(slytherin)).
 mago(draco,puro,odia(hufflepuff)).
 mago(hermione,muggle,_).
@@ -9,28 +11,50 @@ caracteristicas(hermione,[inteligente,orgulloso,responsable]).
 esSangrePura(Sangre):-
     Sangre \= muggle.
 
+% Punto 1
+permiteEntrar(Mago,slytherin):-
+    mago(Mago,Sangre,_),
+    esSangrePura(Sangre).
+
+permiteEntrar(Mago, Casa):-
+    mago(Mago,_,_),
+    Casa \= slytherin.
+
+% Punto 2
 puedeIrA(Mago,gryffindor):-
-    mago(Mago,_,Casa),
+    mago(Mago,_,_),
     caracteristicas(Mago,Caracteristicas),
     member(corajudo,Caracteristicas).
 
 puedeIrA(Mago,slytherin):-
-    mago(Mago,Sangre,Casa),
+    mago(Mago,Sangre,_),
     caracteristicas(Mago,Caracteristicas),
     member(orgulloso,Caracteristicas),
     member(inteligente,Caracteristicas),
     esSangrePura(Sangre).
 
 puedeIrA(Mago,ravenclaw):-
-    mago(Mago,_,Casa),
+    mago(Mago,_,_),
     caracteristicas(Mago,Caracteristicas),
     member(responsable,Caracteristicas),
     member(inteligente,Caracteristicas).
 
 puedeIrA(Mago,hufflepuff):-
-    mago(Mago,_,Casa),
+    mago(Mago,_,_),
+    esAmistoso(Mago,Caracteristicas).
+
+esAmistoso(Mago,Caracteristicas):-
     caracteristicas(Mago,Caracteristicas),
     member(amistoso,Caracteristicas).
 
-permiteEntrar(Mago,slytherin):-
-    not(esSangreSucia(Mago)).
+% Punto 3
+enQueCasaQueda(Mago,Casa):-
+    mago(Mago,Sangre,CasaQueOdia),
+    puedeIrA(Mago,Casa),
+    permiteEntrar(Mago,Casa),
+    CasaQueOdia\=Casa.
+
+% Punto 4
+cadenaDeAmistades(Magos):-
+    findall(Mago,esAmistoso(Mago,Caracteristicas),Magos).
+
